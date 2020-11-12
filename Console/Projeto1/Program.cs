e356de0cafa4374491a9bf3e196369bd7bd4a049
Marcos Roberto <marcosrobertto46@gmail.com>
2020-11-12 19:29:12 -0300

    Projeto 1 Console realizado

---
+++ Console/Projeto1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            string num1_texto = Console.ReadLine();
+            int num1 = int.Parse(num1_texto);
+
+            Console.Write("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+
+            Console.Write("O resultado é: ");
+            Console.WriteLine(soma);
+        }
+    }
+}
