1b613da40e7ae32826276d9e789259d1767754dd
Marcos Roberto <marcosrobertto46@gmail.com>
2020-11-12 22:14:54 -0300

    Semaforo

---
+++ Console/Semaforo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Semaforo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cor do semaforo");
+            string cor = (Console.ReadLine());
+
+            if(cor == "Vermelho" | cor == "vermelho") {
+                Console.WriteLine("Pare o carro agora.");
+            } else if(cor == "Amarelo" | cor == "amarelo") {
+                Console.WriteLine("Diminua a velocidade e pare o carro agora.");
+            } else{
+                Console.WriteLine("Continue acelerando");
+            }
+
+
+        }
+    }
+}
